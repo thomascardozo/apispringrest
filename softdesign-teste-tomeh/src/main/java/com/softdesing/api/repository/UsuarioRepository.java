cce94abb72dc287eaa4c0dbc4a72138f39360135
tomeh <thomas.cardozo@gmail.com>
2020-08-05 17:17:44 -0300

    Atualizacao

--- softdesign-teste-tomeh/src/main/java/com/softdesing/api/repository/UsuarioRepository.java
+++ softdesign-teste-tomeh/src/main/java/com/softdesing/api/repository/UsuarioRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.softdesing.api.model.Usuario;
 
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+	
   Optional<Usuario> findByUsername(String username);
 
   Boolean existsByUsername(String username);
